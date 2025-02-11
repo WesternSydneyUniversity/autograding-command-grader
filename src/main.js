8bc2a26c17e05ef4b193a8c1cfeca024c04a71ad
Tomas Trescak <tomi.trescak@gmail.com>
2025-02-11 14:50:37 +1100

    Default directory

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ function run() {
   const command = core.getInput('command', {required: true})
   const timeout = parseFloat(core.getInput('timeout') || 10) * 60000 // Convert to minutes
   const maxScore = parseInt(core.getInput('max-score') || 0)
-  const workingDirectory = core.getInput('working-directory')
+  const workingDirectory = core.getInput('working-directory') || "."
 
   const processEnv = {
     ...process.env,
